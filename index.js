47eaef68d86489da327ecf99cdd45b5d7a958e73
Mark Vayngrib <mark.vayngrib@lablz.com>
2015-01-08 17:46:01 -0500

    initial commit

---
+++ index.js
@@ -0,0 +1,91 @@
+
+'use strict';
+
+var Q = require('q');
+var assert = require('assert');
+var querystring = require('querystring');
+var request = require('request');
+var get = require('simple-get');
+// var utils = require('tradle-utils');
+var noop = function() {};
+
+function KeeperAPI(config) {
+  if (typeof config === 'string') {
+    var parts = config.split(':');
+    this._host = parts[0];
+    this._port = parts[1];
+  }
+  else {
+    this._host = config.host;
+    this._port = config.port;
+  }
+}
+
+KeeperAPI.prototype.baseUrl = function() {
+  var protocol = this._host.indexOf('://') === -1 ? 'http://' : '';
+  return protocol + this._host + ':' + this._port + '/';
+}
+
+KeeperAPI.prototype.urlFor = function(key) {
+  return this.baseUrl() + 'get?' + querystring.stringify({ key: key });
+}
+
+KeeperAPI.prototype.put = function(key, value, callback) {
+  if (Buffer.isBuffer(key)) key = key.toString('hex');
+  
+  request({
+    method: 'PUT',
+    body: value,
+    url: this.baseUrl() + 'put?' + querystring.stringify({ key: key })
+  }, callback || noop);
+}
+
+// separate requests for now
+KeeperAPI.prototype.getMany = function(keys, callback) {
+  var self = this;
+  var tasks = keys.map(function(k) {
+    return Q.ninvoke(self, 'getOne', k);
+  })
+
+  Q.allSettled(tasks)
+  .then(function(results) {
+    results = results.map(function(r) { return r.value });
+    callback(null, results);
+  })
+  .catch(callback);
+}
+
+KeeperAPI.prototype.getOne = function(key, callback) {
+  callback = callback || noop;
+  
+  // var params = {
+  //   keys: typeof keys === 'string' ? keys : keys.join(',')
+  // };
+
+  get.concat(this.urlFor(key), function(err, data, res) {
+    if (err) return callback(err);
+
+    if (res.statusCode !== 200) {
+      var msg = 'Failed to get data from keeper';
+      if (data.length)
+        msg = JSON.parse(data.toString()).message;
+
+      err = new Error(msg);
+      err.code = res.statusCode;
+
+      return callback(err);
+    }
+
+    callback(null, data);
+  });
+}
+
+// KeeperAPI.prototype._put = function(method, key, value, callback) {
+//   // request.post({
+//   //   headers: {'content-type' : 'application/x-www-form-urlencoded'},
+//   //   url: this.baseUrl() + method,
+//   //   body: querystring.stringify(params)
+//   // }, callback || noop);
+// }
+
+module.exports = KeeperAPI;
